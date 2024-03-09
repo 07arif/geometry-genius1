fc65c40cf406b3fdd9d103fc263a79d65b6c7018
07arif <ai.arif0711@gmail.com>
2024-03-09 21:14:49 +0600

    get triangle base and height from the card

---
+++ scripts/are-calculate.js
@@ -0,0 +1,14 @@
+function calculateTriangleAre() {
+  //   console.log('button clicked');
+  // get triangle base value
+  const baseField = document.getElementById('triangle-base');
+  const baseValueText = baseField.value;
+  const base = parseFloat(baseValueText);
+  console.log(base);
+
+  // get triangle height value
+  const heightField = document.getElementById('triangle-height');
+  const heightValueText = heightField.value;
+  const height = parseFloat(heightValueText);
+  console.log(height);
+}
